2f866c44b5e06feb6dec558b70e62b7668ae51cf
yiwei <46142239+yiweishu@users.noreply.github.com>
2019-01-26 16:38:35 +0800

    tt

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var app = new Vue({ 
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
 });
 
 var app2 = new Vue({
@@ -27,6 +27,13 @@ var app4 = new Vue({
       { text: '学习 Vue' },
       { text: '整个牛项目' }
     ]
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
   }
 })
 
